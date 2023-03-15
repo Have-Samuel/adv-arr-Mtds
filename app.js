f6c81e35dfd0773128562189c96b4f524e81a9a1
Have Samuel <samiehave9@gmail.com>
2023-03-16 00:20:11 +0300

    Applying the filter method

--- app.js
+++ app.js
@@ -1,69 +1,96 @@
-// ForEach Method
-const colors = ['teal', 'red', 'green', 'magenta'];
+// // ForEach Method
+// const colors = ['teal', 'red', 'green', 'magenta'];
 
-function forEach(arr, callback) {
-  for (let i = 0; i < arr.length; i++) {
-    callback(arr[i], i);
-  }
-}
+// function forEach(arr, callback) {
+//   for (let i = 0; i < arr.length; i++) {
+//     callback(arr[i], i);
+//   }
+// }
 
-forEach(colors, (colors, i) => {
-  console.log(colors.toUpperCase(), 'at inidex of', i);
-});
+// forEach(colors, (colors, i) => {
+//   console.log(colors.toUpperCase(), 'at inidex of', i);
+// });
 
-colors.forEach(colors, (i) => {
-  console.log(colors.toUpperCase(), 'at inidex of', i);
-});
+// colors.forEach(colors, (i) => {
+//   console.log(colors.toUpperCase(), 'at inidex of', i);
+// });
 
-// Mapping on Numbers
-const numbers = [21, 32, 54, 78, 89, 109];
-const negatives = numbers.map((num) => num * -1);
+// // Mapping on Numbers
+// const numbers = [21, 32, 54, 78, 89, 109];
+// const negatives = numbers.map((num) => num * -1);
 
-// Mapping on Strings
-const todos = [
-  {
-    id: 1,
-    text: 'Walk the Dog!',
-    priority: 'High',
-  },
-  {
-    id: 1,
-    text: 'Walk the Cat!',
-    priority: 'Medium',
-  },
-  {
-    id: 1,
-    text: 'Walk the Chickens!',
-    priority: 'Low',
-  },
-  {
-    id: 1,
-    text: 'Clean the Garage!',
-    priority: ' Very High',
-  },
+// // Mapping on Strings
+// const todos = [
+//   {
+//     id: 1,
+//     text: 'Walk the Dog!',
+//     priority: 'High',
+//   },
+//   {
+//     id: 1,
+//     text: 'Walk the Cat!',
+//     priority: 'Medium',
+//   },
+//   {
+//     id: 1,
+//     text: 'Walk the Chickens!',
+//     priority: 'Low',
+//   },
+//   {
+//     id: 1,
+//     text: 'Clean the Garage!',
+//     priority: ' Very High',
+//   },
+// ];
+
+// const todoText = todos.map((todo) => todo.text);
+
+// // Mapping on Links
+// const links = Array.from(document.querySelectorAll('a'));
+
+// const urls = links.map((a) => a.href);
+
+// // My own mapping
+// function myMap(arr, callback) {
+//   const mappedArray = [];
+//   for (let i = 0; i < arr.length; i + 1) {
+//     const val = callback(arr[i], i, arr);
+//     mappedArray.push(val);
+//   }
+//   return mappedArray;
+// }
+
+// const priorityMap = myMap(todos, function(todo) {
+//   return todo.priority;
+// });
+
+// const repeatedStrings = myMap(['a', 'b', 'c', 'd', 'e'], function(str, idx) {
+//   return str.repeat(idx);
+// });
+
+// Filters
+const words = [
+  'immunoelectrophoretically',
+  'rotavator',
+  'tsktsk',
+  'psychophysicotherapeutics',
+  'squirrelled',
+  'crypt',
+  'uncopyrightable',
+  'cysts',
+  'pseudopseudohypoparathyroidism',
+  'unimaginatively',
 ];
 
-const todoText = todos.map((todo) => todo.text);
+// Alternative way to write the filter function
+// words.filter((word) => {
+//   if (word.length >= 20) {
+//     return true;
+//   }
+//   return false;
+// });
 
-// Mapping on Links
-const links = Array.from(document.querySelectorAll('a'));
+const longWords = words.filter((word) => word.length > 20);
 
-const urls = links.map((a) => a.href);
-
-// My own mapping
-function myMap(arr, callback) {
-  const mappedArray = [];
-  for (let i = 0; i < arr.length; i + 1) {
-    const val = callback(arr[i], i, arr);
-    mappedArray.push(val);
-  }
-  return mappedArray;
-}
-
-const priorityMap = myMap(todos, function(todo) {
-  return todo.priority;
-});
-
-const repeatedStrings = myMap(['a', 'b', 'c', 'd', 'e'], function(str, idx) {
-  return str.repeat(idx);
-});
+// finding words that start with letters c & u
+const cWOruWords.filter((w) => w[0] === 'u' || w[0] ==='c');
