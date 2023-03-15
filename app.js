518bbcf0e5d366b7f45f629cace5d34290281371
Have Samuel <samiehave9@gmail.com>
2023-03-15 03:21:20 +0300

    Import code and use Map

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ForEach Method
 const colors = ['teal', 'red', 'green', 'magenta'];
 
 function forEach(arr, callback) {
@@ -12,4 +13,41 @@ forEach(colors, (colors, i) => {
 
 colors.forEach(colors, (i) => {
   console.log(colors.toUpperCase(), 'at inidex of', i);
+});
+
+// Map
+const numbers = [21, 32, 54, 78, 89, 109];
+const negatives = numbers.map((num) => num * -1);
+
+const todos = [
+  {
+    id: 1,
+    text: 'Walk the Dog!',
+    priority: 'High',
+  },
+  {
+    id: 1,
+    text: 'Walk the Cat!',
+    priority: 'Medium',
+  },
+  {
+    id: 1,
+    text: 'Walk the Chickens!',
+    priority: 'Low',
+  },
+  {
+    id: 1,
+    text: 'Clean the Garage!',
+    priority: ' Very High',
+  },
+];
+
+const todoText = todos.map((todo) => {
+  return todo.text;
+});
+
+const links = Array.from(document.querySelectorAll('a'));
+
+const urls = links.map(function(a) {
+  return a.href;
 });
