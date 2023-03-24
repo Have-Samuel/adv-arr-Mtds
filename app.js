06edf04acdb9aedcab38189cfd707017ba859b88
Have Samuel <samiehave9@gmail.com>
2023-03-24 04:22:09 +0300

    Writing ES2015 version code

--- app.js
+++ app.js
@@ -452,12 +452,30 @@ createInstructor('Have', 'Peace');
 // }
 
 // ES2015 VERSION
-const instructor = {
-  firstName: 'Colt',
-  sayHi() {
-    return 'Hi';
-  },
-  sayBye() {
-    return `${this.firstName} says bye!`;
-  },
-};
+// const instructor = {
+//   firstName: 'Colt',
+//   sayHi() {
+//     return 'Hi';
+//   },
+//   sayBye() {
+//     return `${this.firstName} says bye!`;
+//   },
+// };
+// CREATE ANIMAL FUNCTION
+// EARLIER VERSION
+const d = createAnimal('dog', 'bark', 'Woooof!');
+// {species: "dog", bark: ƒ}
+d.bark(); // "Woooof!"
+
+const s = createAnimal('sheep', 'bleet', 'BAAAAaaaa');
+// {species: "sheep", bleet: ƒ}
+s.bleet(); // "BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    },
+  };
+}
