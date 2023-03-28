92e6ea94694b200877e9790f582f2f7f54b4ec5e
Have Samuel <samiehave9@gmail.com>
2023-03-29 02:02:59 +0300

    Using Map, has, and sets on vowelCount function

--- app.js
+++ app.js
@@ -990,3 +990,32 @@ const hasDuplicate = (arr) => new Set(arr).size !== arr.length; // removes dupli
 
 hasDuplicate([1, 3, 2, 1]); // true
 hasDuplicate([1, 5, -1, 4]); // false
+
+// *************************************
+// VOWELCOUNT
+// *************************************
+// Write a function called vowelCount which accepts a string and
+// returns a map where the keys are numbers and the values are the
+// count of the vowels in the string.
+
+function isVowel(char) {
+  return 'aeiou'.includes(char);
+}
+
+function vowelCount(str) {
+  const vowelMap = new Map();
+  for (const char of str) {
+    const lowerCaseChar = char.toLowerCase();
+    if (isVowel(lowerCaseChar)) {
+      if (vowelMap.has(lowerCaseChar)) {
+        vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+      } else {
+        vowelMap.set(lowerCaseChar, 1);
+      }
+    }
+  }
+  return vowelMap;
+}
+
+vowelCount('awesome'); // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+vowelCount('Colt'); // Map { 'o' => 1 }
